462d88706bded9aff183b3a01b78f39072a506a5
Omolemo Mashigo <omomashigo@gmail.com>
2025-03-11 19:48:01 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Omolemo Mashigo on 2025/03/11.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
