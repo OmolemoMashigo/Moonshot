bbcf74a96121fd05faab1fd02a1afe6ca79f3902
Omolemo Mashigo <omomashigo@gmail.com>
2025-03-12 22:40:49 +0200

    working with codable JSON data

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
+struct User: Codable{
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable{
+    let street: String
+    let city: String
+}
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Decode JSON"){
+            let input = """
+                {
+                    "name": "Taylor Swift",
+                    "address": {
+                            "street": "555, Taylor ave",
+                            "city": "NYC"
+                    }
+                }
+                """
+            
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            
+            if let user = try? decoder.decode(User.self, from: data){
+                print(user.address.street)
+            }
         }
-        .padding()
     }
 }
 
