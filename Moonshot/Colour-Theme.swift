4f22fc130b362369988efc09b1e537ee4923abc4
Omolemo Mashigo <omomashigo@gmail.com>
2025-03-14 22:48:18 +0200

    formatted date and added custom colours by extending ShapeStyle

--- Moonshot/Colour-Theme.swift
+++ Moonshot/Colour-Theme.swift
@@ -5,4 +5,14 @@
 //  Created by Omolemo Mashigo on 2025/03/14.
 //
 
-import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color{ //extend ShapeStyle where its being used as a Color
+    static var darkBackground: Color{
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground: Color{
+        Color(red: 0.2, green: 0.2, blue: 0.3)
+    }
+}
